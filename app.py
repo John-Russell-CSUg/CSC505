748cb984c2a7d35ca177b6dca7eed7886f05b4bb
Skaster87 <skaster87@hotmail.com>
2022-01-16 20:25:32 -0700

    Finsih work Commit

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, redirect, url_for, render_template, request, session, flash
-from datetime import timedelta
+from datetime import timedelta, datetime
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import DateTime
 
 app = Flask(__name__)
 # Session Setup
@@ -12,26 +13,55 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
-# class Entity(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     created_at = db.Column(DateTime)
-#     updated_at = db.Column(DateTime)
-#     last_updated_by = db.Column(db.String)
-#
-#     def __init__(self, created_by):
-#         self.created_at = datetime.now()
-#         self.updated_at = datetime.now()
-#         self.last_updated_by = created_by
-
 
 class users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column("name", db.String(100))
     email = db.Column("email", db.String(100))
+    address = db.Column("address", db.String(100))
+    phone = db.Column("phone", db.String(100))
 
     def __init__(self, name, email):
         self.name = name
         self.email = email
+
+
+class pothole_report(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    street_address = db.Column("street_address", db.String(100))
+    size = db.Column("size", db.Integer)
+    priority = db.Column("priority", db.Integer)
+    location = db.Column("location", db.String(100))
+    damage_type = db.Column("damage_type", db.String(100))
+    user = db.Column("user", db.String(100))
+
+    def __init__(self, street_address, location, damage_type, size):
+        self.street_address = street_address
+        self.location = location
+        self.damage_type = damage_type
+        self.size = size
+        self.priority = int(size) * 2
+        if "user" in session:
+            user_name = session["user"]
+        self.user = user_name
+
+
+class work_order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    report_id = db.Column(db.Integer)
+    equipment = db.Column("equipment", db.String(100))
+    crew = db.Column("repair_crew", db.String(100))
+    hours = db.Column("hours", db.Integer)
+    filler = db.Column("filler", db.Integer)
+    cost = db.Column("cost", db.Integer)
+
+    def __init__(self, report_id, equipment, crew, hours, filler):
+        self.report_id = report_id
+        self.equipment = equipment
+        self.crew = crew
+        self.hours = hours
+        self.filler = filler
+        self.cost = (int(hours) * int(crew)) + (int(filler) * 2)
 
 
 @app.route("/")
@@ -83,23 +113,76 @@ def user():
         user_name = session["user"]
         if request.method == "POST":
             email = request.form["email"]
+            address = request.form["address"]
+            phone = request.form["phone"]
             session["email"] = email
             # Get User from DB
             found_user = users.query.filter_by(name=user_name).first()
             found_user.email = email
+            found_user.address = address
+            found_user.phone = phone
             db.session.commit()
-            flash("Email was saved")
+            flash("Info was saved")
         else:
             if "email" in session:
                 email = session["email"]
-        return render_template("user.html", email=email)
+                found_user = users.query.filter_by(name=user_name).first()
+                address = found_user.address
+                phone = found_user.phone
+        return render_template("user.html", email=email, address=address, phone=phone)
     else:
         flash("You are not logged in")
         return redirect(url_for("login"))
 
+
 @app.route("/view_users")
 def view_users():
     return render_template("view_users.html", values=users.query.all())
+
+
+@app.route("/report_pothole", methods=["POST", "GET"])
+def new_report():
+    if request.method == "POST":
+        address = request.form["address"]
+        location = request.form["location"]
+        damage_type = request.form["damage_type"]
+        size = request.form["size"]
+        report = pothole_report(address, location, damage_type, size)
+        db.session.add(report)
+        db.session.commit()
+        flash("Report Created")
+        return render_template("report_pothole.html")
+
+    else:
+        return render_template("report_pothole.html")
+
+
+@app.route("/view_pothole")
+def view_pothole_report():
+    return render_template("view_pothole_report.html", values=pothole_report.query.all())
+
+
+@app.route("/create_workorder/<report_id>", methods=["POST", "GET"])
+def create_workorder(report_id):
+    if request.method == "POST":
+        global message
+        equipment = request.form["equipment"]
+        crew = request.form["crew"]
+        hours = request.form["hours"]
+        filler = request.form["filler"]
+        new_workorder = work_order(report_id, equipment, crew, hours, filler)
+        db.session.add(new_workorder)
+        db.session.commit()
+        flash("WorkOrder Created")
+        return view_pothole_report()
+    else:
+        report = pothole_report.query.filter_by(id=report_id).first()
+        return render_template("create_workorder.html", id=report_id, street_address=report.street_address)
+
+@app.route('/view_workorders')
+def view_workorder():
+    return render_template("view_workorders.html", values=work_order.query.all())
+
 
 if __name__ == "__main__":
     db.create_all()
--- enums.py
+++ enums.py
@@ -1,7 +1,9 @@
+import enum
 from enum import Enum
 
+
 # region Enums so user can enter only int without typing string
-class damage_type_enum(Enum):
+class damage_type_enum(enum.Enum):
     Crack = 0
     Chip = 1
     Destroyed = 2
@@ -12,18 +14,26 @@ class damage_type_enum(Enum):
         for i in location_enum:
             print("\t", i.name, " = ", i.value)
 
+    def __str__(self):
+        return str(self.value)
 
-class hole_status_enum(Enum):
+
+class hole_status_enum(enum.Enum):
     In_Progress = 0
     Repaired = 1
     Not_Repaired = 2
     Temporary_Repair = 3
 
+    def __str__(self):
+        return str(self.value)
 
-class location_enum(Enum):
+
+class location_enum(enum.Enum):
     middle = 0
     curb = 1
     sidewalk = 2
 
+    def __str__(self):
+        return str(self.value)
 
 # endregion Enums
